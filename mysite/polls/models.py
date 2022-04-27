8cd0e93e4581495ca5297d308c35f343d53a0832
jubaaitidir <jubaaitidir@gmail.com>
2022-04-27 14:23:14 +0200

    first part of polls/test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 
+from django.utils import timezone
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
@@ -14,7 +15,7 @@ class IndexView(generic.ListView):
     template_name='polls/index.html'
     context_object_name= 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     
     
     
